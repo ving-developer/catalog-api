7a183fb2fd6c8fc2bea918ab3793ba57dfa4cac8
ving-developer <henrique.sb0709@gmail.com>
2022-05-15 17:52:54 -0300

    Adding Controllers

---
+++ Catalog-API/Catalog-API/Controllers/CategoriesController.cs
@@ -0,0 +1,92 @@
+﻿using Catalog_API.Models;
+using Catalog_API.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog_API.Controllers;
+
+[ApiController]// Configure attribute routing requirement, ModelState validation, ModelBinding parameter inference (automatically adding [FromBody] to POST methods) and Automatic HTTP 400 responses.
+[Route("[controller]")]//Sets controller route
+public class CategoriesController : Controller
+{
+    private readonly IUnityOfWork _unity;
+
+    public CategoriesController(IUnityOfWork unity)
+    {
+        _unity = unity;
+    }
+
+    [HttpGet("products")]
+    public ActionResult<IEnumerable<Category>> GetCategoriesProducts()
+    {
+        var categories = _unity.CategoryRepository.GetCategoryProducts().ToList();
+        if (categories is not null)
+        {
+            return categories;
+        }
+        return NotFound();
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Category>> Get()
+    {
+        var categories = _unity.CategoryRepository.Get().ToList();
+        if (categories is null)
+        {
+            return NotFound();
+        }
+
+        return categories;
+    }
+
+    [HttpGet("{id:int}", Name = "GetCategoryById")]
+    public ActionResult<Category> Get(int id)
+    {
+        var category = _unity.CategoryRepository.GetById(c => c.CategoryId == id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+        return Ok(category);
+    }
+
+    [HttpPost]
+    public ActionResult Post(Category category)
+    {
+        _unity.CategoryRepository.Add(category);
+        _unity.Commit();
+        return new CreatedAtRouteResult("GetCategoryById",
+            new { id = category.CategoryId }, category);
+        /* This method returns code 201 (created result) and also adds a
+         * "location" field in the response Header, with the URI used to
+         * query the created category (good practices of REST).
+         * "GetCategoryById" parameter corresponds to the internal
+         * name of the route that will be called to query the created category*/
+    }
+
+    [HttpPut("{id:int}")]
+    public ActionResult Put(int id, Category category)
+    {
+        if (id != category.CategoryId)
+        {
+            return BadRequest();
+        }
+
+        _unity.CategoryRepository.Update(category);
+        _unity.Commit();
+        return Ok(category);
+    }
+
+    [HttpDelete("{id:int}")]
+    public ActionResult Delete(int id)
+    {
+        var categoria = _unity.CategoryRepository.GetById(c => c.CategoryId == id);
+        if (categoria is not null)
+        {
+            _unity.CategoryRepository.Delete(categoria);
+            _unity.Commit();
+            return Ok(categoria);
+        }
+
+        return NotFound();
+    }
+}
---
+++ Catalog-API/Catalog-API/Controllers/ProductsController.cs
@@ -0,0 +1,81 @@
+﻿using Catalog_API.Models;
+using Catalog_API.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog_API.Controllers;
+
+[ApiController]// Configure attribute routing requirement, ModelState validation, ModelBinding parameter inference (automatically adding [FromBody] to POST methods) and Automatic HTTP 400 responses.
+[Route("[controller]")]//Sets controller route
+public class ProductsController : Controller
+{
+    private readonly IUnityOfWork _unity;
+
+    public ProductsController(IUnityOfWork unity)
+    {
+        _unity = unity;
+    }
+
+    [HttpGet("price")]
+    public ActionResult<IEnumerable<Product>> ListProductsByPrice()
+    {
+        return _unity.ProductRepository.ListProductsByPrice().ToList();
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Product>> Get()
+    {
+        var products = _unity.ProductRepository.Get().Take(10).ToList();// The Take() method will define a limit of products to be searched
+        if (products is null)
+        {
+            return NotFound();
+        }
+        return products;
+    }
+
+    [HttpGet("{id:int}", Name = "GetProduct")]//Specifies that this route will receive an id attribute of type integer and adds an internal name for this route. In this example this route name is being used in the CreatedAtRouteResult method
+    public ActionResult<Product> Get(int id)
+    {
+        var product = _unity.ProductRepository.GetById(p => p.ProductId == id);
+        if (product is null)
+        {
+            return NotFound();
+        }
+        return product;
+    }
+    
+    [HttpPost]
+    public IActionResult Post(Product product)//IActionResult in the return type means that this method will only return ActionResult, that is, responses with status codes and without objects in the Request Body
+    {
+        _unity.ProductRepository.Add(product);
+        _unity.Commit();
+
+        return CreatedAtRoute("GetProduct", new { id = product.ProductId}, product);
+        /* This method returns the code 201 (created result) and also adds a "location" field
+         * in the response header, with the URI used to query the created object (good practices of REST).
+         * The "GetProduct" parameter corresponds to the name of the route that will be called
+         * to query the created object*/
+    }
+
+    [HttpPut("{id:int}")]
+    public ActionResult<Product> Put(int id, Product product)
+    {
+        if (id != product.ProductId) return BadRequest();
+
+        _unity.ProductRepository.Update(product);
+        _unity.Commit();
+        return Ok(product);
+    }
+
+    [HttpDelete("{id:int}")]
+    public ActionResult<Product> Delete(int id)
+    {
+        var product = _unity.ProductRepository.GetById(p => p.ProductId == id);
+        if (product is null)
+        {
+            return NotFound();
+        }
+        _unity.ProductRepository.Delete(product);
+        _unity.Commit();
+        return Ok(product);
+    }
+}
