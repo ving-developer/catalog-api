e3e08ad1f3bcdfbffd97bd07bc44cee3ec770b33
ving-developer <henrique.sb0709@gmail.com>
2022-05-15 22:10:25 -0300

    Adding DTO Design Pattern
    It is not good practice to return to view our Model classes. This can expose the endpoints to unwanted information, or limit the inclusion of extra information for the endpoints. By using the DTO Pattern, we enable the Model and DTO to grow individually

---
+++ Catalog-API/Catalog-API/DTOs/CategoryDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Catalog_API.DTOs;
+
+public class CategoryDTO
+{
+    public int CategoryId { get; set; }
+
+    public string Name { get; set; }
+
+    public string ImageUrl { get; set; }
+
+    public ICollection<ProductDTO> Products { get; set; }
+}
---
+++ Catalog-API/Catalog-API/DTOs/ProductDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace Catalog_API.DTOs
+{
+    public class ProductDTO
+    {
+        public int ProductId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public float Price { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public CategoryDTO Category { get; set; }
+    }
+}
